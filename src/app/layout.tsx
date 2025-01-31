63afb1d192da9daaaafa2b4832d4652e2e2021e3
Daniel Chyliński <danielosd68@gmail.com>
2025-01-31 20:11:55 +0100

    start

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,30 @@
-import type { Metadata } from "next";
 import "./globals.css";
-import {createTheme, CssBaseline} from "@mui/material";
-import {ThemeProvider} from "@mui/system";
+import {CssBaseline, ThemeProvider} from "@mui/material";
+import {Metadata} from "next";
+import {AppRouterCacheProvider} from "@mui/material-nextjs/v13-appRouter";
+import theme from "invoiceo/theme";
 
 
 export const metadata: Metadata = {
-  title: "Invoiceo.pl - Wystaw fakturę w 5 minut",
-  description: "Platforma do płynnego wystawiania, opłacania i zarządzania fakturami.",
+    title: "Invoiceo.pl - Wystaw fakturę w 5 minut",
+    description: "Platforma do płynnego wystawiania, opłacania i zarządzania fakturami.",
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+    children,
+   }: Readonly<{
+    children: React.ReactNode;
 }>) {
     return (
-    <html lang="en">
-      <body>
-      <CssBaseline />
-      {children}
-      </body>
-    </html>
-  );
+        <html lang="pl">
+        <body className={'font-[nunito]'}>
+        <AppRouterCacheProvider>
+            <CssBaseline/>
+            <ThemeProvider theme={theme}>
+                {children}
+            </ThemeProvider>
+        </AppRouterCacheProvider>
+        </body>
+        </html>
+    );
 }
