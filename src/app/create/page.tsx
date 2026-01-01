58d94f53970ad4e43d85f1ae84c4727ece4927d0
Daniel Chyliński <danielosd68@gmail.com>
2026-01-01 12:34:19 +0100

    fix

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,19 +1,9 @@
 "use client"
-import React, {useEffect} from 'react';
+import React from 'react';
 import InvoiceCreatorTemplate from "invoiceo/components/templates/invoiceCreatorTemplate/InvoiceCreatorTemplate";
 import PageWrapper from "invoiceo/components/organisms/pageWrapper/PageWrapper";
-import * as invoiceModel from "invoiceo/data.json";
 
 const Page = () => {
-
-
-    useEffect(() => {
-        const invoiceDataFromLocalStorage = localStorage.getItem('INVOICEO_INVOICE_MODEL');
-
-        if(invoiceDataFromLocalStorage === null){
-            localStorage.setItem('INVOICEO_INVOICE_MODEL', JSON.stringify(invoiceModel))
-        }
-    }, []);
 
     return (
         <PageWrapper>
